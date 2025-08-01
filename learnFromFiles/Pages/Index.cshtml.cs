ab6e55e076a633b805ab7dc40d17afe48f0b0380
Joaquin Monteagudo <joaquisubriam@gmail.com>
2025-08-01 17:22:28 +0200

    adding  glob vars and better method for export

--- learnFromFiles/Pages/Index.cshtml.cs
+++ learnFromFiles/Pages/Index.cshtml.cs
@@ -14,11 +14,15 @@ public class IndexModel : PageModel
     [BindProperty]
     public IFormFile? Upload { get; set; }
     [BindProperty]
-   public string SearchKey { get; set; } = "";
-   public string ActualKey { get; private set; } = "";
+    public string SearchKey { get; set; } = "";
+    [BindProperty]
+    public IDictionary<string, string> SearchResultTmp { get; private set; } = new Dictionary<string, string>();
 
-    public IEnumerable<string> Files { get; private set; } = Enumerable.Empty<string>(); //filel ist 
-   public IDictionary<string, string> SearchResults { get; private set; } = new Dictionary<string, string>();
+    [BindProperty]
+    public string FileNameToExport { get; set; } = "";
+
+    public IEnumerable<string> Files { get; private set; } = Enumerable.Empty<string>();
+    public IDictionary<string, string> SearchResults { get; private set; } = new Dictionary<string, string>();
 
     public IndexModel(ILogger<IndexModel> logger, FileService fileService)
     {
@@ -30,26 +34,7 @@ public class IndexModel : PageModel
     {
         Files = _fileService.GetAllFileNames();
     }
-    public IActionResult OnPost()
-    {
-        if (!ModelState.IsValid)
-            return Page();
 
-        Usuario.Greeting = $"¡Hola, {Usuario.Name}! Bienvenido/a a Razor Pages.";
-        return Page();  // re‑renderiza con la propiedad Saludo actualizada
-    }
-        public async Task<IActionResult> OnPostUploadAsync()
-        {
-
-        if (Upload != null && Upload.Length > 0)
-        {
-            Console.WriteLine($"we are uploading file with {Upload.FileName}");
-            await _fileService.SaveFileAsync(Upload);
-        }
-            // Recargar lista de archivos
-            Files = _fileService.GetAllFileNames();
-            return RedirectToPage();
-        }
 
     public IActionResult OnPostSearch()
     {
@@ -57,9 +42,43 @@ public class IndexModel : PageModel
         if (!string.IsNullOrWhiteSpace(SearchKey))
         {
             SearchResults = _fileService.SearchWithLine(SearchKey);
-            ActualKey = SearchKey ;
+            SearchResultTmp = SearchResults;
+        }
+
+        return Page();
+    }
+
+    public IActionResult OnPostExport()
+    {
+
+        if (SearchResultTmp == null || SearchResultTmp.Count == 0)
+        {
+            ModelState.AddModelError(string.Empty, "No hay resultados para exportar.");
+            return Page(); // or show a message
         }
         
-        return Page();
+        var payload = _fileService.ExportSearchToFile(SearchResultTmp, FileNameToExport);
+
+        return File(payload.Content, payload.ContentType, payload.DownloadName);
+    }
+        public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+            return Page();
+
+        Usuario.Greeting = $"¡Hola, {Usuario.Name}! Bienvenido/a a Razor Pages.";
+        return Page();  
+    }
+    public async Task<IActionResult> OnPostUploadAsync()
+    {
+
+        if (Upload != null && Upload.Length > 0)
+        {
+            Console.WriteLine($"we are uploading file with {Upload.FileName}");
+            await _fileService.SaveFileAsync(Upload);
+        }
+        // Recargar lista de archivos
+        Files = _fileService.GetAllFileNames();
+        return RedirectToPage();
     }
 }
