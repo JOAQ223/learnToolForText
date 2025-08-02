eecc647a2b86d4a0a11db7bed368ee6a1ac6f72c
Joaquin Monteagudo <joaquisubriam@gmail.com>
2025-08-02 16:16:39 +0200

    4523f23fcd2-Adding karten page to the navbar and file for kart

---
+++ learnFromFiles/Pages/Karten.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using learnFromFiles.Models;
+using learnFromFiles.Services;
+
+namespace learnFromFiles.Pages
+{
+
+    public class Karten : PageModel
+    {
+
+
+    }
+}
